29140bb35227b1d6ad7c0cdc77ea12b00213b140
rutamt <95180713+rutamt@users.noreply.github.com>
2023-06-11 09:35:26 -0400

    Created a .gitignore, everything works well. Rounds work, reset works, everything basically done

--- main.py
+++ main.py
@@ -1,11 +1,46 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template
 import random
 import csv
 import requests
 import html
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = "secret"
+PEXELS_API_KEY = os.environ.get("PEXELS_API_KEY")
+
+
+def get_place_info(place_name):
+    # Retrieve place summary from Wikipedia API
+    wikipedia_endpoint = (
+        f"https://en.wikipedia.org/api/rest_v1/page/summary/{place_name}"
+    )
+    response = requests.get(wikipedia_endpoint)
+    if response.status_code == 200:
+        data = response.json()
+        summary = data.get("extract", "")
+    else:
+        summary = ""
+
+    # Retrieve place photo from Pexels API
+    pexels_endpoint = "https://api.pexels.com/v1/search"
+    headers = {"Authorization": PEXELS_API_KEY}
+    params = {"query": place_name, "per_page": 1}
+    response = requests.get(pexels_endpoint, headers=headers, params=params)
+    if response.status_code == 200:
+        data = response.json()
+        if data.get("total_results", 0) > 0:
+            photo = data["photos"][0]["src"]["large"]
+        else:
+            photo = ""
+    else:
+        photo = ""
+
+    return summary, photo
 
 
 def get_random_landmark():
@@ -32,7 +67,6 @@ def get_random_landmark():
             new_data = response.json()
 
         if new_data:
-            print("data: ", data)
             landmark_lat = float(new_data["lat"])
             landmark_lon = float(new_data["lon"])
             return landmark_name, landmark_lat, landmark_lon
@@ -43,11 +77,15 @@ def get_random_landmark():
 @app.route("/", methods=["GET", "POST"])
 def index():
     landmark_name, landmark_lat, landmark_lon = get_random_landmark()
+    summary, photo = get_place_info(landmark_name)
+
     return render_template(
         "index.html",
         landmark_name=landmark_name,
         landmark_lat=landmark_lat,
         landmark_lon=landmark_lon,
+        summary=summary,
+        photo=photo,
     )
 
 
--- static/js/index.js
+++ static/js/index.js
@@ -2,10 +2,24 @@ var map = L.map("map").setView([0, 0], 2);
 var marker;
 var score = localStorage.getItem('score');
 var rounds = localStorage.getItem('rounds');
-var rounds = localStorage.getItem('rounds');
+let distance = parseFloat(localStorage.getItem('distance'));
 
 const totalScoreElement = document.getElementById('total-score');
 const totalScore = localStorage.getItem('score');
+const statsElement = document.getElementById('stats');
+
+
+// console.log("Rounds", rounds)
+var roundScore = 1000 * rounds;
+// console.log("RS ", roundScore);
+const guessAcuracy = (totalScore / roundScore) * 100;
+
+// Filling in the game statistics when page is loaded
+document.addEventListener('DOMContentLoaded', function () {
+    // Call the function once the document has finished loading
+    statsElement.innerHTML = `<pre>Rounds: ${rounds}\nAverage distance: ${Math.round((distance / rounds) * 100) / 100} Km\nAccuracy: ${Math.round(guessAcuracy * 100) / 100}%</pre>`;
+
+});
 
 if (rounds === null) {
     rounds = 1;
@@ -13,9 +27,14 @@ if (rounds === null) {
     rounds = parseInt(rounds);
 }
 
-console.log("Rounds", rounds)
-var roundScore = 1000 * rounds;
-console.log("RS ", roundScore);
+if (isNaN(distance)) {
+    localStorage.setItem('distance', 0);
+    distance = 0;
+} else {
+    distance = parseInt(distance);
+}
+
+
 
 console.log("Changing totalScore");
 totalScoreElement.textContent = `Total Score: ${totalScore} / ${roundScore}`;
@@ -28,25 +47,45 @@ if (score === null) {
 
 
 // Update and save the score
-function updateScore(newScore) {
+function updateScore(newScore, newDistance) {
     var currentScore = parseInt(localStorage.getItem('score'));
     currentScore += newScore;
     localStorage.setItem('score', currentScore);
+
     var currentRounds = parseInt(localStorage.getItem('rounds')) + 1;
     localStorage.setItem('rounds', currentRounds);
+
+    var currentDistance = parseInt(localStorage.getItem('distance'));
+    currentDistance += newDistance;
+    localStorage.setItem('distance', currentDistance);
+    console.log(`CD: ${currentDistance}, td = ${distance}`);
+
     console.log("Rounds from US", localStorage.getItem('rounds'))
     document.getElementById('total-score').textContent = 'Score: ' + currentScore + ' / ' + 1000 * currentRounds;
 }
 
+var finalTextElement = document.getElementById('final-score-text');
+var scoreModal = document.getElementById('final-modal');
+var resetButton = document.getElementById('reset-score-btn');
+const submitButton = document.getElementById('submit-btn');
+const lookingFor = document.getElementById('landmark-info');
 
 // Clear the score from localStorage
 function clearScore() {
+    scoreModal.style.display = 'block';
+    submitButton.style.display = 'none';
+    resetButton.style.display = 'none';
+    lookingFor.style.display = 'none';
+    map.off('click');
+    finalTextElement.innerHTML = `<pre>Rounds: ${rounds}\nAverage distance: ${Math.round((distance / rounds) * 100) / 100} Km\nAccuracy: ${Math.round(guessAcuracy * 100) / 100}%</pre>`;
+
     score = 0;
     rounds = 1;
     document.getElementById('total-score').textContent = 'Score: ' + score + ' / ' + 1000 * rounds;
     localStorage.setItem('score', score);
+    localStorage.setItem('distance', score);
+
     localStorage.setItem('rounds', rounds);
-    location.reload(); // Reload the page to update the displayed score
 }
 
 
@@ -99,15 +138,17 @@ function calculateScore(lat, lon) {
 
     const distanceText = document.getElementById('distance-from-point');
     const meterDistance = (Math.round((distance * 1000) * 100) / 100);
+    const formattedDistance = (Math.round(distance * 100) / 100);
 
-    distanceText.textContent = `${(Math.round(distance * 100) / 100).toLocaleString("en-US")}KM (${(Math.round((distance * 0.621371) * 100) / 100).toLocaleString("en-US")}Mi) or ${meterDistance.toLocaleString("en-US")}M (${(Math.round((meterDistance * 3.2808) * 100) / 100).toLocaleString("en-US")}ft)`;
+    distanceText.textContent = `${meterDistance >= 1000 ? `${(Math.round(distance * 100) / 100).toLocaleString("en-US")}KM (${(Math.round((distance * 0.621371) * 100) / 100).toLocaleString("en-US")}Mi)` : `${meterDistance.toLocaleString("en-US")}M (${(Math.round((meterDistance * 3.2808) * 100) / 100).toLocaleString("en-US")}ft)`}`;
 
     const congratsLine = document.getElementById('congrats-text');
-    if (score >= 750) {
+    if (score === 1000) { congratsLine.textContent = `Perfect score! Can you do it again?` }
+    else if (score >= 750) {
         congratsLine.textContent = `Great job! Only ${1000 - score} point${1000 - score == 1 ? "" : "s"} from perfection!`
     }
 
-    updateScore(parseInt(formattedScore));
+    updateScore(parseInt(formattedScore), formattedDistance);
     scoreModal.style.display = 'block';
 
     var greenIcon = new L.Icon({
@@ -137,4 +178,40 @@ function calculateScore(lat, lon) {
 }
 
 
-// Call the showIntroModal function to check if the modal should be displayed
+function showModal() {
+    document.getElementById('stats-modal').classList.add('active');
+}
+
+function hideModal() {
+    document.getElementById('stats-modal').classList.remove('active');
+}
+
+var img = document.getElementById('landmark-photo');
+
+img.addEventListener('load', function () {
+    var maxWidth = 600;
+    var maxHeight = 500;
+
+    if (img.naturalWidth > maxWidth || img.naturalHeight > maxHeight) {
+        var aspectRatio = img.naturalWidth / img.naturalHeight;
+        var width = img.naturalWidth;
+        var height = img.naturalHeight;
+
+        if (aspectRatio > 1) {
+            // Landscape image
+            if (img.naturalWidth > maxWidth) {
+                width = maxWidth;
+                height = maxWidth / aspectRatio;
+            }
+        } else {
+            // Portrait image
+            if (img.naturalHeight > maxHeight) {
+                height = maxHeight;
+                width = maxHeight * aspectRatio;
+            }
+        }
+
+        img.width = width;
+        img.height = height;
+    }
+});
